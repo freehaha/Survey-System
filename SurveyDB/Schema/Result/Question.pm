dfd081d14d9baf2d98c818f5e268c5353b3f1b49
Freehaha <freehaha@gmail.com>
2010-07-24 16:46:20 +0800

    var renaming, delete previous answers before add answer

--- SurveyDB/Schema/Result/Question.pm
+++ SurveyDB/Schema/Result/Question.pm
@@ -16,41 +16,43 @@ sub get_options {
 	return \@options;
 }
 
-sub get_answered {
+sub get_user_answered {
 	my ($self, $user) = @_;
 	return undef unless defined $user;
 
 	my $options = $self->get_options;
 	my @ops = map { $_->{oid} } @$options;
 
-	my $result = $self->result_source->schema->resultset('Answer')->search(
+	my $rs = $self->result_source->schema->resultset('Answer')->search(
 		{ 
 			option => \@ops,
 			user => $user
+		},
+		{
+			order_by => { -desc => 'aid' },
 		}
-	)->single;
-	return $result->aid;
-}
-sub delete_answer {
+	);
+	my $ret = $rs->next;
+	#delete those duplicated answers, if exist
+	while(my $row = $rs->next) {
+		$row->delete;
+	}
+	return $ret?$ret->aid:undef;
 }
 sub answer($$) {
 	my ($self, $user, $option) = @_;
 	my $options = $self->get_options;
-	if($user) {
-		$self->result_source->schema->resultset('Answer')->find_or_create(
-			{ 
-				option => $option,
-				user => $user
-			}
-		);
-	} else {
-		$self->result_source->schema->resultset('Answer')->create(
-			{ 
-				option => $option,
-				user => $user
-			}
-		);
+	if($user) { #if already answered, delete it first
+		my $ans = $self->get_user_answered($user);
+		$ans->delete if $ans;
 	}
+
+	$self->result_source->schema->resultset('Answer')->create(
+		{ 
+			option => $option,
+			user => $user
+		}
+	);
 }
 
 1;

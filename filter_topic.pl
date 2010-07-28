3bca5f8c406b623ff067cb6d554a67f6f6c998f1
freehaha@nb <freehaha@gmail.com>
2010-07-28 17:48:41 +0800

    add topic filtering examples

---
+++ filter_topic.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use File::Basename;
+use lib dirname $0;
+use SurveyDB::Schema;
+use Data::Dumper;
+use strict;
+
+my $schema = SurveyDB::Schema->connect('dbi:SQLite:survey.db');
+
+=begin explain
+the behavior here will be:
+	only the keys supplied will be taken into account of the intersection
+	between conditions.
+=cut
+my $topics;
+$topics = $schema->get_topics(
+	bot => 'bot2',
+	chatroom => 'ct1',
+	uid => 1,
+);
+print Dumper($topics);
+$topics = $schema->get_topics(
+	bot => 'bot1',
+	uid => 1,
+	event => 'event1',
+);
+print Dumper($topics);
